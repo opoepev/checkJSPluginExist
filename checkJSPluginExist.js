e5fdb731b1265132465dfa423d339f6508876985
opoepev <opoepev@gmail.com>
2015-03-06 17:46:52 +0800

    Update checkJSPluginExist.js

--- checkJSPluginExist.js
+++ checkJSPluginExist.js
@@ -2,7 +2,7 @@
  *	This code it's help you to check JS plugin function (e.g. jQuery) exist.
  *	When function not exist, the code will auto reload JS plugin from your setting.
  *	
- *  plugin_name: It's your plugin function name (e.g. jQuery). The type is string.
+ *	plugin_name: It's your plugin function name (e.g. jQuery). The type is string.
  *	reload_url: It's your reload plugin function URL. The type is string.
  *	
  *	Copyright 2015, opoepev (Matt, Paul.Lu, Yi-Chun Lu)
